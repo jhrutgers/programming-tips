6fc11cb39a136112408c9f82bfa624803aaa9b6a
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2021-09-26 00:04:59 +0200

    add tip

---
+++ src/20210927_compare.cpp
@@ -0,0 +1,133 @@
+﻿/*
+ * Three-way-comparison operator
+ *
+ * Things can be compared using the operators == != < > <= >=.  C++20
+ * introduces the three-way comparison <=>. Actually, it saves you a lot of
+ * work.
+ */
+
+// The operator <=> is part of the language. The ordering types below are
+// defined in this header.
+#include <compare>
+
+// These are just for this example.
+#include <iostream>
+#include <limits>
+#include <string_view>
+using std::literals::string_view_literals::operator""sv;
+
+class Gas {
+};
+
+class Country {
+public:
+	double households{};
+	double gas_storage_capacity_kWh{};
+	double average_usage_kWhpy{std::numeric_limits<double>::quiet_NaN()};
+
+	double max_gas_reserve_per_household() const { return gas_storage_capacity_kWh / households; }
+
+	// We only implement two of them...
+	bool operator==(Country const& o) const
+	{
+		return this == &o || max_gas_reserve_per_household() == o.max_gas_reserve_per_household();
+	}
+
+	bool operator<(Country const& o) const
+	{
+		return max_gas_reserve_per_household() < o.max_gas_reserve_per_household();
+	}
+
+	// ...and derive the rest.
+	bool operator!=(Country const& o) const { return !(*this == o); }
+	bool operator<=(Country const& o) const { return *this < o || *this == o; }
+
+	// Not true when max_gas_reserve_per_household() would return nan, as
+	// that would result in a partial ordering (neither object would be
+	// less or greater than the other), which this implementation does not
+	// take into account.
+	bool operator>(Country const& o) const { return !(*this <= o); }
+	bool operator>=(Country const& o) const { return !(*this < o); }
+};
+
+class NewWay {
+public:
+	std::strong_ordering operator<=>(NewWay const& o) const
+	{
+		return std::strong_ordering::equal;
+	}
+};
+
+class Storage {
+public:
+	std::string_view name;
+	double capacity_TWh{};
+	double max_withdrawal_rate_GWhpd{};
+	double max_injection_rate_GWhpd{};
+
+	std::partial_ordering operator<=>(Storage const& o) const
+	{
+		auto a = capacity_TWh;
+		auto b = o.capacity_TWh;
+
+		return
+			a == b ? std::partial_ordering::equivalent :
+			a < b  ? std::partial_ordering::less :
+			b < a  ? std::partial_ordering::greater :
+			         std::partial_ordering::unordered; // such as NaN.
+
+		// return a <=> b; would be easier...
+	}
+};
+
+class DefaultWay {
+public:
+	auto operator<=>(DefaultWay const& o) const = default;
+};
+
+int main()
+{
+	// Can't compare, as there is no comparison operator defined for Gas.
+//	Gas natural;
+//	Gas green;
+//	std::cout << std::boolalpha << "natural == green ?  " << (natural == green) << std::endl;
+
+	// Designated initializers! Finally!
+	Country netherlands{.households = 7.9e6, .gas_storage_capacity_kWh = 144.6e9, .average_usage_kWhpy = 12'104};
+	Country belgium{5.0e6, 9.0e9, 23'260};
+	Country uk(27.8e6, 17.5e9, 12'000); // Looks strange since '20, huh?
+
+	std::cout << "Max gas reserve per household in The Netherlands is " <<
+		netherlands.max_gas_reserve_per_household() << " kWh (average usage is " <<
+		netherlands.average_usage_kWhpy << " kWh/y) (assuming nothing is reserved for industry)" << std::endl;
+
+	std::cout << "Belgium has " <<
+		belgium.max_gas_reserve_per_household() << " kWh (average usage is " <<
+		belgium.average_usage_kWhpy << " kWh/y)" << std::endl;
+
+	std::cout << "Belgium has " << (belgium < netherlands ? "less" : "more") << " then The Netherlands" << std::endl;
+
+	std::cout << "UK has " <<
+		uk.max_gas_reserve_per_household() << " kWh (average usage is " <<
+		uk.average_usage_kWhpy << " kWh/y)" << std::endl;
+
+	Storage NL0201{"Grijpskerk"sv, 27.67, 719.33, 172.92};
+	Storage NL0202{"Norg"sv, 59.34, 791.18, 448.75};
+	Storage UK1102{"Stublach"sv, std::numeric_limits<double>::quiet_NaN(), 170.0, 170.0};
+
+	std::cout << (NL0201 <=> NL0202 < 0) << std::endl;
+	std::cout << (NL0201 < NL0202) << std::endl;
+	std::cout << (NL0201 > NL0202) << std::endl;
+
+	std::cout << (NL0201 <=> UK1102 == 0) << std::endl;
+	std::cout << (NL0201 <=> UK1102 < 0) << std::endl;
+	std::cout << (NL0201 <=> UK1102 > 0) << std::endl;
+}
+
+/*
+ * Further reading:
+ *
+ * https://en.cppreference.com/w/cpp/language/operator_comparison#Three-way_comparison
+ * https://en.cppreference.com/w/cpp/language/default_comparisons
+ */
+
